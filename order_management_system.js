8eff59ffd5805738a70d5a0273c9d8eef824a50c
Fernanda <fernanda19@usf.edu>
2024-09-19 15:44:08 -0400

    Task 1: Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,7 @@
+// task 1: create inventory array
+const inventory = [
+    { name: 'Espresso', price: 3, quantity: 10 },
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+];
